62148a608cb6703568b2e02bb20f16f3d2b0d241
MikaelFiil <mikael.fiil@netbizz.dk>
2025-03-19 09:37:20 +0100

    Showcase for GANTT chart not displaying weeks, when adding both days, weeks and months

--- src/main/java/com/example/application/views/ganttchart/GANTTChartView.java
+++ src/main/java/com/example/application/views/ganttchart/GANTTChartView.java
@@ -1,36 +1,136 @@
 package com.example.application.views.ganttchart;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.charts.model.style.SolidColor;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
-import org.vaadin.lineawesome.LineAwesomeIconUrl;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
 
 @PageTitle("GANTT Chart")
-@Route("")
-@Menu(order = 0, icon = LineAwesomeIconUrl.FILE)
+@Route(value = "GANTT", layout = MainLayout.class)
+@Menu(order = 1, icon = "line-awesome/svg/home-solid.svg")
+@AnonymousAllowed
 public class GANTTChartView extends VerticalLayout {
+
+    private static final Instant TODAY = Instant.now()
+            .truncatedTo(ChronoUnit.DAYS);
 
     public GANTTChartView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        Chart chart = new Chart(ChartType.GANTT);
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        final Configuration configuration = chart.getConfiguration();
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        configuration.setTitle("Gantt Chart with custom labels");
+        configuration.setTooltip(new Tooltip(true));
+        configuration.getxAxis().setCurrentDateIndicator(true);
+
+        XAxis xAxis = configuration.getxAxis();
+        xAxis.setMinPadding(0.05);
+        xAxis.setMaxPadding(0.05);
+
+        YAxis yAxis = configuration.getyAxis();
+        yAxis.setCategories("Prototyping", "Development", "Testing");
+
+        PlotOptionsGantt plotOptionsGantt = new PlotOptionsGantt();
+        plotOptionsGantt.setShowCheckbox(true);
+        configuration.setPlotOptions(plotOptionsGantt);
+
+        // tag::snippet[]
+        final GanttSeries projectDevelopmentSeries = createProjectDevelopmentSeries();
+        PlotOptionsGantt seriesPlotOptions = new PlotOptionsGantt();
+        var dataLabels = new ArrayList<DataLabels>();
+
+        var assigneeLabel = new DataLabels(true);
+        assigneeLabel.setAlign(HorizontalAlign.LEFT);
+        assigneeLabel.setFormat("{point.custom.assignee}");
+        dataLabels.add(assigneeLabel);
+
+        var endDateLabel = new DataLabels(true);
+        endDateLabel.setAlign(HorizontalAlign.RIGHT);
+        endDateLabel.setFormat("{point.end:%e. %b}");
+        dataLabels.add(endDateLabel);
+
+        var avatarLabel = new DataLabels(true);
+        avatarLabel.setAlign(HorizontalAlign.LEFT);
+        avatarLabel.setUseHTML(true);
+        avatarLabel.setFormat(
+                "<div style=\"width: 20px; height: 20px; overflow: hidden; margin-left: -30px\">"
+                        + "                <img src=\"https://ui-avatars.com/api/?background=random&color=fff&size=20&length=1&rounded=true&name={point.custom.assignee}\"> "
+                        + "                </div>");
+        // dataLabels.add(avatarLabel);
+
+        seriesPlotOptions.setDataLabels(dataLabels);
+        projectDevelopmentSeries.setPlotOptions(seriesPlotOptions);
+        // end::snippet[]
+        configuration.addSeries(projectDevelopmentSeries);
+
+        add(chart);
+    }
+
+    private GanttSeries createProjectDevelopmentSeries() {
+        GanttSeries series = new GanttSeries();
+        series.setName("Project 1");
+
+        GanttSeriesItem item;
+
+        item = new GanttSeriesItem(0, todayPlus(1), todayPlus(3));
+        item.setCustom(new TaskCustomData("John"));
+        item.setCompleted(new Completed(0.15, SolidColor.LIGHTBLUE));
+        series.add(item);
+
+        item = new GanttSeriesItem(1, todayPlus(2), todayPlus(5));
+        item.setCustom(new TaskCustomData("William"));
+        item.setCompleted(new Completed(0.25, SolidColor.LIGHTBLUE));
+        series.add(item);
+
+        item = new GanttSeriesItem(2, todayPlus(5), todayPlus(7));
+        item.setCustom(new TaskCustomData("Jane"));
+        item.setCompleted(new Completed(0.35, SolidColor.LIGHTBLUE));
+        series.add(item);
+
+        item = new GanttSeriesItem(1, todayPlus(8), todayPlus(16));
+        item.setCustom(new TaskCustomData("John"));
+        item.setCompleted(new Completed(0.45, SolidColor.LIGHTBLUE));
+        series.add(item);
+
+        item = new GanttSeriesItem(2, todayPlus(10), todayPlus(23));
+        item.setCustom(new TaskCustomData("Jane"));
+        item.setCompleted(new Completed(0.55, SolidColor.LIGHTBLUE));
+        series.add(item);
+
+        return series;
+    }
+
+    private Instant todayPlus(int days) {
+        return TODAY.plus(days, ChronoUnit.DAYS);
+    }
+
+    @SuppressWarnings("unused")
+    static class TaskCustomData extends AbstractConfigurationObject {
+        private String assignee;
+
+        public TaskCustomData(String assignee) {
+            this.assignee = assignee;
+        }
+
+        public String getAssignee() {
+            return assignee;
+        }
+
+        public void setAssignee(String assignee) {
+            this.assignee = assignee;
+        }
     }
 
 }
---
+++ src/main/java/com/example/application/views/ganttchart/GANTTChartView2.java
@@ -0,0 +1,123 @@
+package com.example.application.views.ganttchart;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
+import com.vaadin.flow.component.charts.model.style.SolidColor;
+import com.vaadin.flow.component.charts.model.style.Style;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+
+
+@PageTitle("GANTT Chart 2")
+@Route(value = "GANTT 2", layout = MainLayout.class)
+@Menu(order = 2, icon = "line-awesome/svg/home-solid.svg")
+@AnonymousAllowed
+public class GANTTChartView2 extends VerticalLayout {
+
+    public GANTTChartView2() {
+        setSpacing(false);
+        add(createOpenBidRequestTimeline());
+    }
+
+    private void configureYearsMonthsWeeksOnXAxis(Configuration configuration) {
+        configureDaysAxis(configuration);
+        //configureWeeksAxis(configuration);          // Setting week together with days will make a mess :-)
+        configureMonthsAxis(configuration);
+    }
+
+    private void configureDaysAxis(Configuration configuration) {
+        XAxis axis = new XAxis();
+        configuration.addxAxis(axis);
+        axis.setMinPadding(0.05);
+        axis.setMaxPadding(0.05);
+        axis.setUnits(new TimeUnitMultiples(TimeUnit.DAY, 1));
+
+        final Labels labels = new Labels();
+        labels.setPadding(1);
+        labels.setAlign(HorizontalAlign.LEFT);
+        axis.setLabels(labels);
+
+        axis.setGrid(new AxisGrid());
+        axis.getGrid().setCellHeight(20);
+    }
+
+    private void configureWeeksAxis(Configuration configuration) {
+        XAxis axis = new XAxis();
+        configuration.addxAxis(axis);
+        axis.setMinPadding(0.05);
+        axis.setMaxPadding(0.05);
+        axis.setUnits(new TimeUnitMultiples(TimeUnit.WEEK, 1));
+
+        final Labels labels = new Labels();
+        labels.setPadding(1);
+        labels.setAlign(HorizontalAlign.LEFT);
+        axis.setLabels(labels);
+
+        axis.setGrid(new AxisGrid());
+        axis.getGrid().setCellHeight(20);
+    }
+
+    private void configureMonthsAxis(Configuration configuration) {
+        XAxis axis = new XAxis();
+        configuration.addxAxis(axis);
+        axis.setMinPadding(0.05);
+        axis.setMaxPadding(0.05);
+        axis.setTickInterval(1000 * 60 * 60 * 24 * 30L);
+        axis.setUnits(new TimeUnitMultiples(TimeUnit.MONTH, 1));
+
+        final Labels labels = new Labels();
+        labels.setAlign(HorizontalAlign.LEFT);
+        var style = new Style();
+        style.setFontSize("16px");
+        labels.setStyle(style);
+        axis.setLabels(labels);
+
+        axis.setGrid(new AxisGrid());
+        axis.getGrid().setCellHeight(20);
+    }
+
+
+    private Component createOpenBidRequestTimeline() {
+        Div content = new Div();
+        content.setSizeFull();
+        content.add(bidRequestGantt());
+        return content;
+    }
+
+
+    private Chart bidRequestGantt()  {
+        Chart chart = new Chart(ChartType.GANTT);
+        chart.setSizeFull();
+
+// Modify the default configuration
+        Configuration configuration = chart.getConfiguration();
+        configuration.setTitle("Active Bid Request Timeline");
+        configureYearsMonthsWeeksOnXAxis(configuration);
+
+// Add data
+        GanttSeries series = new GanttSeries("");
+        PlotOptionsGantt plotOptionsGantt = new PlotOptionsGantt();
+        series.setPlotOptions(plotOptionsGantt);
+
+        GanttSeriesItem bidGanttItem = new GanttSeriesItem("ATEA NAS II",
+            LocalDateTime.now().toInstant(ZoneOffset.ofHours(1)).truncatedTo(ChronoUnit.DAYS),
+            LocalDateTime.now().plusDays(23).toInstant(ZoneOffset.ofHours(1)).truncatedTo(ChronoUnit.DAYS));
+        bidGanttItem.setColor(SolidColor.CADETBLUE);
+        bidGanttItem.setCompleted(new Completed(25, new SolidColor("#0f728f")));
+        series.add(bidGanttItem);
+
+        configuration.addSeries(series);
+        return chart;
+    }
+
+}
